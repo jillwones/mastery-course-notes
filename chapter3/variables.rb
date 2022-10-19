2f67e48a726f8b60ba97a40ca573dafdc375c14b
williamjones <willj836@gmail.com>
2022-10-19 16:10:29 +0100

    updated variables, floats etc and dot syntax with parentheses

---
+++ chapter3/variables.rb
@@ -0,0 +1,15 @@
+# Convert your numeral calculator to use dot syntax with parentheses
+
+# Make the first few floats rather than integers
+ONE = 1.to_f
+TWO = 2.to_f
+THREE = 3.0
+FOUR = 4
+FIVE = 5
+SIX = 6
+SEVEN = 7
+EIGHT = 8
+NINE = 9
+TEN = 10
+
+puts ONE.+(ONE)
